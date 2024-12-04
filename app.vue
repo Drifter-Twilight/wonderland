48f712cd798bbafe7521bad8e3cedf301ee3d264
Drifter-Twilight <2696259805@qq.com>
2024-12-04 10:21:37 +0800

    feat: 添加第三方库

--- app.vue
+++ app.vue
@@ -1,6 +1,13 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div ref='divRef' class="w-[300px] h-[300px] bg-blue-300">
+    <a-button>点击</a-button>
   </div>
 </template>
+
+<script setup>
+const div = useTemplateRef('divRef')
+
+onMounted(() => {
+  console.log(div)
+})
+</script>
