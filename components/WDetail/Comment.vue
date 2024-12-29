d2c200117ab90295af2da4dabdc10aaf11a8f94c
Drifter-Twilight <2696259805@qq.com>
2024-12-29 18:21:14 +0800

    feat: 添加评论功能

--- components/WDetail/Comment.vue
+++ components/WDetail/Comment.vue
@@ -1,46 +1,28 @@
 <template>
-  <div class="box-border border-t border-solid border-gray-300 py-4">
-    <div class="flex-center mb-2">
-      <div class="flex-center w-10 h-10 rounded-full text-lg"><icon-robot /></div>
-      <a-textarea placeholder="Please enter something" auto-size allow-clear class="min-h-20" />
-    </div>
-
-    <div class="flex justify-end">
-      <a-button status="success" @click.prevent="loginGithub">登录github以进行评论</a-button>
-    </div>
-  </div>
-
-  <a-comment
-    author="Socrates"
-    avatar="https://p1-arco.byteimg.com/tos-cn-i-uwbnlip3yd/3ee5f13fb09879ecb5185e440cef6eb9.png~tplv-uwbnlip3yd-webp.webp"
-    content="Comment body content."
-    datetime="1 hour">
-    <template #actions>
-      <span class="action"><IconMessage />回复</span>
-    </template>
-  </a-comment>
+  <Giscus
+    id="comments"
+    repo="Drifter-Twilight/wonderland"
+    repo-id="R_kgDONYmVjA"
+    category="Comment"
+    categoryid="DIC_kwDONYmVjM4Clllu"
+    mapping="specific"
+    :term="title || null || undefined"
+    reactionsenabled="1"
+    emitmetadata="0"
+    inputposition="top"
+    theme="light"
+    lang="zh-CN"
+    loading="lazy"
+    async />
 </template>
 
 <script setup lang="ts">
-const { data, signIn } = useAuth()
+import Giscus from '@giscus/vue'
+import { useTitle } from '@vueuse/core';
+import 'giscus';
 
-
-function loginGithub() {
-// async function loginGithub() {
-  // await signIn('github')
-  console.log(data)
-}
+const title = useTitle()
 </script>
 
 <style scoped>
-:deep(.arco-textarea-wrapper) {
-  background-color: #ddd;
-  color: dimgray;
-}
-
-:deep(.arco-textarea-wrapper:focus-within, .arco-textarea-wrapper.arco-textarea-focus) {
-  border-color: #D69340;
-  background-color: #ccc;
-  color: #333;
-}
 </style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,25 +6,8 @@ export default defineNuxtConfig({
     '@nuxtjs/tailwindcss',
     'arco-design-nuxt-module',
     '@vueuse/nuxt',
-    '@nuxt/content',
-    '@sidebase/nuxt-auth'
+    '@nuxt/content'
   ],
-  auth: {
-    isEnabled: true,
-    disableServerSideAuth: false,
-    originEnvKey: process.env.NUXT_AUTH_ORIGIN,
-    baseURL: 'https://drifter-twilight.github.io/wonderland/api/auth',
-    provider: { 
-      type: 'authjs',
-      trustHost: false,
-      defaultProvider: 'github',
-      addDefaultCallbackUrl: true
-    },
-    sessionRefresh: {
-      enablePeriodically: true,
-      enableOnWindowFocus: true,
-    }
-  },
   css: ['animate.css', '@/main.css', '@/assets/css/transition.css', '@/assets/css/content.css'],
   arco: {
     importPrefix: 'A',
@@ -46,10 +29,4 @@ export default defineNuxtConfig({
   app: {
     baseURL: process.env.NODE_ENV === 'production' ? '/wonderland/' : '/'
   },
-  runtimeConfig: {
-    authSecret: '',
-    authOrigin: '',
-    clientId: '',
-    clientSecret: '',
-  }
 })
